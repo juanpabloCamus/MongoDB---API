8966bd55bb4e9ca5b293c32d577d59b0e6d22284
Juan Pablo <juanpablocamus28@gmail.com>
2022-09-08 15:08:36 -0300

    users hashed

--- API/src/controllers/users.js
+++ API/src/controllers/users.js
@@ -1,13 +1,16 @@
 const usersRouter = require('express').Router();
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
 usersRouter.post('/', async (req, res) => {
     const {username, name, password} = req.body;
 
+    const passwordHash = await bcrypt.hash(password, 10)
+
     const user = new User({
         username,
         name,
-        passwordHash: password
+        passwordHash
     })
 
     const savedUser = await user.save()
